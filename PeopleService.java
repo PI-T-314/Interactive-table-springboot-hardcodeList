5a080a0a134bed32c22d5bb2e42282ac7183ac48
Naeem Rmaity <133520187+PI-T-314@users.noreply.github.com>
2023-07-17 19:12:24 +0300

    PeopleService.java

--- PeopleService.java
+++ PeopleService.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,10 +18,10 @@ public class PeopleService {
 	
 	
 	public PeopleService(People people) {
-		list.add((new People("Ali", 32, "Male", "Web Developer")));
-		list.add((new People("Dana", 25, "Female", "Pharmacist")));
-		list.add((new People("Ahmad", 27, "Male", "Engineer")));
-		list.add((new People("Tala", 29, "Female", "Journalist")));
+		list.add((new People(12, "Ali", 32, "Male", "Web Developer")));
+		list.add((new People(132, "Dana", 25, "Female", "Pharmacist")));
+		list.add((new People(445, "Ahmad", 27, "Male", "Engineer")));
+		list.add((new People(68, "Tala", 29, "Female", "Journalist")));
 	}
 	
 	public String sayHello() {
@@ -33,6 +34,16 @@ public class PeopleService {
 
 	public void addPerson(People people) {
 		list.add(people);
+		
+	}
+
+	public void deletePerson(int people) {
+		
+		for(int i=0; i<list.size();i++) {
+			if(people == list.get(i).getId()) {
+				list.remove(i);
+			}
+		}
 		
 	}
 
