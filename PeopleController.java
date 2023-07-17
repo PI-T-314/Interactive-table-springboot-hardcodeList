510a89985f030e88767a4b9b479d0323071ad051
Naeem Rmaity <133520187+PI-T-314@users.noreply.github.com>
2023-07-17 19:10:46 +0300

    PeopleController.java

--- PeopleController.java
+++ PeopleController.java
@@ -5,9 +5,11 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -49,6 +51,12 @@ public class PeopleController {
 	@RequestMapping(value="/register", method = RequestMethod.POST)
 	public String registerDone(@ModelAttribute("people") People people) {
 		ps.addPerson(people);
+		return "redirect:/people-table";
+	}
+	
+	@GetMapping("/delete-registered")
+	public String deleteregister(@RequestParam int peopleid) {
+		ps.deletePerson(peopleid);
 		return "redirect:/people-table";
 	}
 	
